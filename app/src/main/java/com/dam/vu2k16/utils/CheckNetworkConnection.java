e87e08bc5a7264a7ad0bb3656c201c71b639f838
Pranav V R <pvrkites@gmail.com>
2016-12-24 00:48:46 +0530

    updated dec 24

---
+++ app/src/main/java/com/dam/vu2k16/utils/CheckNetworkConnection.java
@@ -0,0 +1,7 @@
+package com.dam.vu2k16.utils;
+
+/**
+ * Created by pvr on 23/12/16.
+ */
+public class CheckNetworkConnection {
+}
---
+++ app/src/main/java/com/dam/vu2k16/utils/ShareAppViaIntent.java
@@ -0,0 +1,7 @@
+package com.dam.vu2k16.utils;
+
+/**
+ * Created by pvr on 24/12/16.
+ */
+public class ShareAppViaIntent {
+}
